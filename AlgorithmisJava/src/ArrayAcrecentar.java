a7480df738fb26d12b9c8858466b7329d962931f
Fabio <fabio_dev@outlook.com>
2021-02-15 20:43:47 -0300

    Update

--- AlgorithmisJava/src/ArrayAcrecentar.java
+++ AlgorithmisJava/src/ArrayAcrecentar.java
@@ -1,19 +1,26 @@
 public class ArrayAcrecentar {
     public static void main(String[] args) {
 
+        //cria um vetor
         int[] vetor = {90, 70, 50, 80, 60, 85};
 
+        //cria vetor auxiliar, que recebe o vetor principal com uma posição a mais
         int[] vetorAux = new int[vetor.length + 1];
 
-        for (int i = 0; i < vetor.length; i++){
+        //adiciona o vetor principal no vetor auxiliar
+        for (int i = 0; i < vetor.length; i++) {
             vetorAux[i] = vetor[i];
 
         }
+        //adiciona um valor ao novo indice
         vetorAux[vetor.length] = 75;
         vetor = vetorAux;
 
+        //imprime o array com o novo indice adicionado
         for (int i = 0; i < vetor.length; i++){
-            System.out.println(vetor[i]);
+            System.out.print(vetor[i] + ", ");
+
+            //saida: 90, 70, 50, 80, 60, 85, 75,
 
         }
     }
