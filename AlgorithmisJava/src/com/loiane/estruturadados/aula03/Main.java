6f191c8fe99dde61c90d0c3f209cb36862600601
Fabio <fabio_dev@outlook.com>
2021-02-20 17:51:58 -0300

    Update

---
+++ AlgorithmisJava/src/com/loiane/estruturadados/aula03/Main.java
@@ -0,0 +1,21 @@
+package com.loiane.estruturadados.aula03;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(10);
+        vetor.adicioina(1);
+        vetor.adicioina(2);
+        vetor.adicioina(3);
+        vetor.adicioina(40);
+        vetor.adicioina(50);
+
+
+        System.out.println(vetor.adiciona());
+
+
+
+    }
+
+
+}
