f8a152a858c356daa1a433e20100c9986fc1d7b2
Fabio <fabio_dev@outlook.com>
2021-04-20 21:41:02 -0300

    Update

---
+++ AlgorithmisJava/src/PesquisaDicotomia.java
@@ -0,0 +1,27 @@
+public class PesquisaDicotomia {
+    public static void main(String[] args) {
+
+        int[] vetor = {50, 60, 70, 80, 90};
+
+
+    }
+    // valor hdjahdjsahjdhassakdl
+    public static void dicotomia(int[] vetor){
+
+        int inicio  = vetor[0];
+        int fim = vetor[vetor.length -1];
+        int meio =  (inicio + fim) / 2;
+
+        for(int i = 0; i < vetor.length; i++){
+            //busca menor
+            if (meio > vetor[i]){
+
+            }
+            //busca maior
+            if(meio < vetor[i]){
+
+            }
+        }
+
+    }
+}
